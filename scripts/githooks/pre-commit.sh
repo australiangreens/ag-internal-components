647ee43ada9773fb2e37fa3e28de6d7508d98606
Anthony <anthony@unfinishedteleporter.com>
2023-06-02 14:23:30 +0800

    Attempting to use lint-staged

--- scripts/githooks/pre-commit.sh
+++ scripts/githooks/pre-commit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo -n "Running eslint..."
-if !(yarn --silent eslint); then
+echo -n "Running eslint on staged files..."
+if !(npx lint-staged); then
     echo "Error: eslint failed"
     exit 1
 fi
