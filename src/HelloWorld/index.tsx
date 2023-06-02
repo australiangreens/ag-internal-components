ee6d9d07dd9610f60c3ab0e5bf07203d49c9fe3c
Anthony <anthony@unfinishedteleporter.com>
2023-06-02 16:40:10 +0800

    Ensure material ui is working

--- src/HelloWorld/index.tsx
+++ src/HelloWorld/index.tsx
@@ -1,7 +1,29 @@
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+
 export interface HelloWorldProps {
   text: string;
 }
 
 export default function HelloWorld({ text }: HelloWorldProps) {
-  return <div className="text">This is test text: {text}</div>;
+  const [num, setNum] = useState(0);
+
+  return (
+    <>
+      <Typography variant="h6" color="inherit" component="div" sx={{ fontSize: '22px' }}>
+        Hello world
+      </Typography>
+
+      <Typography variant="caption" display="block">
+        <span>Just ensuring MUI is working as planned as a peer dependency</span>
+      </Typography>
+      <Button variant="outlined" onClick={() => setNum((oldNum) => oldNum + 1)}>
+        Hello I am a button
+      </Button>
+      <p>This is some text: {text}</p>
+      <Paper>This number will incremember when button pressed: {num}</Paper>
+    </>
+  );
 }
