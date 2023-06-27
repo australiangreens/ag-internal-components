4e3442a22604d17dd27492302dc5a2e54d82fc41
Anthony <anthony@unfinishedteleporter.com>
2023-06-27 16:33:59 +0800

    Bugfix: Export PageLayout

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
 export { default as ExampleComponent } from './ExampleComponent';
 export * from './ExampleComponent';
+
+export { default as PageLayout } from './PageLayout';
+export * from './PageLayout';
