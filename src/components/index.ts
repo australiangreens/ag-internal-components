75b3be82326f4603eb8b1fbaa7a4b0a3b15e44e9
Anthony <anthony@unfinishedteleporter.com>
2023-07-07 11:26:57 +0800

    [LIST-476] Move PageLayout into new location

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,5 @@
 export { default as ExampleComponent } from './ExampleComponent';
 export * from './ExampleComponent';
 
-export { default as PageLayout } from './PageLayout';
-export * from './PageLayout';
-
 export { default as NavBar } from './NavBar';
 export * from './NavBar';
