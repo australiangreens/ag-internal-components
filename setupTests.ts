33341346e733325b495678bec76d6f0c647605af
Anthony <anthony@unfinishedteleporter.com>
2023-09-29 14:28:07 +0800

    [EVNT-144] Add jest-extended

--- setupTests.ts
+++ setupTests.ts
@@ -1,12 +1,13 @@
-import matchers from '@testing-library/jest-dom/matchers';
+import { default as jestDomMatchers } from '@testing-library/jest-dom/matchers';
 import { cleanup } from '@testing-library/react';
+import { default as jestExtendedMatchers } from 'jest-extended';
 import { afterEach, expect } from 'vitest';
 
 // This added extra matchers. It works with vitest as well as jest
 import '@australiangreens/ag-error-jest';
 
-// extends Vitest's expect method with methods from react-testing-library
-expect.extend(matchers);
+expect.extend(jestExtendedMatchers);
+expect.extend(jestDomMatchers);
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
