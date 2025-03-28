8943d8e96e872a1aa56cb99df688e2b6e8941569
luke <luke.kugelman@greens.org.au>
2025-03-28 10:54:17 +1000

    [LIST-909] Add util for getting valid business roles from a mapping

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,6 +2,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { IdToken } from '@auth0/auth0-spa-js';
 import { useQuery } from '@tanstack/react-query';
 import { DomainCode } from 'src/domainCode';
+import { T } from 'vitest/dist/chunks/environment.C5eAp3K6';
 
 function fromKebab(string: string) {
   return string.replace(/(^|-)([a-z])/g, (_, separator, char) =>
@@ -52,3 +53,9 @@ export const useValidDomains = (appValidBusinessRoles: string[]) => {
     },
   });
 };
+
+export const getValidBusinessRoles = (roleMapping: Record<T, string[]>) =>
+  Object.values(roleMapping).reduce(
+    (prev, curr) => [...new Set([...prev, ...curr])],
+    [] as string[]
+  );
