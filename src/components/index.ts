8cc64b2c4033a1c9f91fcd0e16bafb0725f61e78
Anthony <anthony@unfinishedteleporter.com>
2025-10-03 15:47:18 +0800

    [LIST-1001] [red] Remove old NavBar component
    Its not used by any apps any more

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,6 @@
 export * from './ExampleComponent';
 export { default as ExampleComponent } from './ExampleComponent';
 
-export * from './NavBar';
-export { default as NavBar } from './NavBar';
 
 export * from './AgDialog';
 export { default as AgDialog } from './AgDialog';
@@ -13,8 +11,9 @@ export { default as FetchAutocomplete } from './FetchAutocomplete';
 export * from './SingleAutocomplete';
 export { default as SingleAutocomplete } from './SingleAutocomplete';
 
-export { default as SessionExpiryDialog } from './SessionExpiryDialog';
 export * from './SessionExpiryDialog';
+export { default as SessionExpiryDialog } from './SessionExpiryDialog';
 
 export * from './AuthGuard';
 export { default as AuthGuard } from './AuthGuard';
+
