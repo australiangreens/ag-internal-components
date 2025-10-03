75316cf1bf1ce0161cd4310737c91dfca3c84f5a
Anthony <anthony@unfinishedteleporter.com>
2025-10-03 12:50:30 +0800

    [LIST-1004] Fix storybook addons etc

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,8 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    // The following 6 addons are included by default as part of
-    // @storybook/addon-essentials. However the only way to configure any of
-    // them is to list them all individually
-    '@storybook/addon-actions',
     {
       name: '@storybook/addon-docs',
       options: {
@@ -13,20 +10,11 @@ const config: StorybookConfig = {
         transcludeMarkdown: true
       }
     },
-    '@storybook/addon-controls',
-    '@storybook/addon-viewport',
-    '@storybook/addon-backgrounds',
-    '@storybook/addon-toolbars',
-
     '@storybook/addon-links',
-    '@storybook/addon-interactions',
   ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
-  },
-  docs: {
-    autodocs: 'tag',
   },
 };
 export default config;
